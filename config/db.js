981242b00b7f0cb0437714f2a28e1ed2a414a58a
Basteen-A <santhiyagubasteen@gmail.com>
2025-02-26 19:28:28 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -1,16 +1,18 @@
-require('dotenv').config();
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: 'bfji7re9uwrpfbbaet7b-mysql.services.clever-cloud.com',         // MySQL server host (default: localhost)
+  database: 'bfji7re9uwrpfbbaet7b',  // Your database name
+  user: 'ucyjc9vjjupjg5cb',             // Replace with your MySQL username (e.g., 'root')
+  password: 'LLc9TNszrn6sqVELqGGR', // Replace with your MySQL password
 });
 
-db.connect(err => {
-  if (err) throw err;
-  console.log('MySQL Connected');
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL database');
 });
 
 module.exports = db;
